ee7e1a698c466a7ddb73ea15de89a044031a017c
Xiaoyi Liu <lxy878@gmail.com>
2020-03-14 14:07:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
   end
 
   def get_courses
-    courses = Nokogiri::XML::NodeSet(get_page)
+    courses = Nokogiri::XML::NodeSet.new(get_page)
     puts courses.class
     # courses.collect do |course|
     #   course
