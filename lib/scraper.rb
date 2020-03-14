72d4434e8323cb8d38323efba598b73ba1200f8f
Xiaoyi Liu <lxy878@gmail.com>
2020-03-14 14:39:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,10 +28,8 @@ class Scraper
     get_courses.each do |course|
       new_course = Course.new
       new_course.title = course.css("h2").text
-      puts course.css(".date").text
-      puts course.css("p").text
-      new_course.schedule
-      new_course.description
+      new_course.schedule = course.css(".date").text
+      new_course.description = course.css("p").text
 
     end
   end
