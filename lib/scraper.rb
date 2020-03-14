4b13a48b5b276a66b264a94dbacaf2f016b0609b
Xiaoyi Liu <lxy878@gmail.com>
2020-03-14 14:21:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -22,7 +22,7 @@ class Scraper
 
   def get_courses
     courses = get_page.css(".post-holder")
-    puts courses.class
+    puts courses.inspect
     # courses.each do |course|
     #   course
     # end
